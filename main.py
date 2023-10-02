74280930be0827177dc34e04cff9e2b99283d5d8
Leonid-poc <legeorg2004@gmail.com>
2023-10-02 18:03:22 +0500

    добавление библиотеки работы с таблицами и добавлени заголовков

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from design import Ui_MainWindow
+import tabulate
 
 class App(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.tableWidget.clicked.connect(self.run)
+        self.headers = ['Дата', 'Номер договора', 'УПД', 'Сумма документа', 'Неоплаченная часть', 'Дата оплаты по договору']
         pass
     
     def getTableText(self, x, y):
@@ -24,10 +26,19 @@ class App(QMainWindow, Ui_MainWindow):
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
     
+data = [
+    ['id', 'name', 'number'],
+    [0, 'Jeff', 1234],
+    [1, 'Bob', 5678],
+    [2, 'Bill', 9123]
+]
+results = tabulate.tabulate(data, tablefmt='grid', headers='firstrow')
+print(results)
+
 # Запуск приложения
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = App()
-    ex.show()
-    sys.excepthook = except_hook
-    app.exec()
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     ex = App()
+#     ex.show()
+#     sys.excepthook = except_hook
+#     app.exec()
