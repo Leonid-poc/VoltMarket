5a8032836c8b13594731002edfa3e327d6fb4f3c
Leonid-poc <legeorg2004@gmail.com>
2023-10-02 16:59:34 +0500

    Написан стандартный шаблон приложения

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from design import Ui_MainWindow
+
+class App(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        
+# Проверка ошибок
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+    
+# Запуск приложения
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    ex.show()
+    sys.excepthook = except_hook
+    app.exec()
